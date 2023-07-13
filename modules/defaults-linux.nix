6535cf4d1b0ffab03af65b01187eb46bc7c3675a
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2023-07-13 17:00:09 +0900

    linux: enable bluetooth audio hd codecs

--- modules/defaults-linux.nix
+++ modules/defaults-linux.nix
@@ -78,12 +78,7 @@ with lib;
   services.pipewire.wireplumber.enable = true;
 
   environment.etc."wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-    bluez_monitor.properties = {
-      ["bluez5.enable-sbc-xq"] = true,
-      ["bluez5.enable-msbc"] = true,
-      ["bluez5.enable-hw-volume"] = true,
-      ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-    }
+    bluez_monitor.properties["bluez5.codecs"] = "[ sbc sbc_xq aac ldac aptx aptx_hd ]"
     '';
 
   environment.etc."pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
