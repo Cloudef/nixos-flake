4614220f6cb6b1d99a4cef5f74e088014f4fd11e
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2023-07-13 16:59:55 +0900

    linux: tune audio latency

--- modules/defaults-linux.nix
+++ modules/defaults-linux.nix
@@ -88,10 +88,9 @@ with lib;
 
   environment.etc."pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
     context.properties = {
-      default.clock.rate = 48000
-        default.clock.quantum = 32
-        default.clock.min-quantum = 32
-        default.clock.max-quantum = 32
+      default.clock.quantum = 128
+      default.clock.min-quantum = 128
+      default.clock.max-quantum = 128
     }
     '';
 
