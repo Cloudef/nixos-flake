bcb84da0181ccd7f701014af689501d4c5501545
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2025-02-03 21:57:22 +0900

    flake: add zig2nix cache

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -24,10 +24,12 @@ with lib;
   nix.settings.substituters = [
     "https://cache.nixos.org"
     "https://nix-community.cachix.org"
+    "https://zig2nix.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "zig2nix.cachix.org-1:LSXEcFKIAN2NtNsznYLMbhEtyHHorV0caI2UgTIxk2k="
   ];
 
   # We in Tokyo
